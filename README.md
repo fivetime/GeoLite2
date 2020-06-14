63d7b196a477ce00694e02d2eb1f25ed6f5ec149
root <root@HK-Database.jdck4jkp44gufbi13grmcqk3ge.hx.internal.cloudapp.net>
2020-06-14 03:33:25 +0000

    20200609

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GeoLite2
 GeoLite2 database
+
+git rm -r --cached .
+git add .
+
+git commit -am '20200609'
+git push
+
---
+++ update.sh
@@ -0,0 +1,17 @@
+. config.sh
+source config.sh
+
+wget -c "$Server?edition_id=GeoLite2-ASN&license_key=$ApiKey&suffix=tar.gz" -O GeoLite2-ASN.tar.gz
+wget -c "$Server?edition_id=GeoLite2-ASN&license_key=$ApiKey&suffix=tar.gz.sha256" -O GeoLite2-ASN.tar.gz.sha256
+wget -c "$Server?edition_id=GeoLite2-ASN-CSV&license_key=$ApiKey&suffix=zip" -O GeoLite2-ASN-CSV.zip
+wget -c "$Server?edition_id=GeoLite2-ASN-CSV&license_key=$ApiKey&suffix=zip.sha256" -O GeoLite2-ASN-CSV.zip.sha256
+
+wget -c "$Server?edition_id=GeoLite2-City&license_key=$ApiKey&suffix=tar.gz" -O GeoLite2-City.tar.gz
+wget -c "$Server?edition_id=GeoLite2-City&license_key=$ApiKey&suffix=tar.gz.sha256" -O GeoLite2-City.tar.gz.sha256
+wget -c "$Server?edition_id=GeoLite2-City-CSV&license_key=$ApiKey&suffix=zip" -O GeoLite2-City-CSV.zip
+wget -c "$Server?edition_id=GeoLite2-City-CSV&license_key=$ApiKey&suffix=zip.sha256" -O GeoLite2-City-CSV.zip.sha256
+
+wget -c "$Server?edition_id=GeoLite2-Country&license_key=$ApiKey&suffix=tar.gz" -O GeoLite2-Country.tar.gz
+wget -c "$Server?edition_id=GeoLite2-Country&license_key=$ApiKey&suffix=tar.gz.sha256" -O GeoLite2-Country.tar.gz.sha256
+wget -c "$Server?edition_id=GeoLite2-Country-CSV&license_key=$ApiKey&suffix=zip" -O GeoLite2-Country-CSV.zip
+wget -c "$Server?edition_id=GeoLite2-Country-CSV&license_key=$ApiKey&suffix=zip.sha256" -O GeoLite2-Country.zip.sha256
